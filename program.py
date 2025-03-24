54f0446d6c6e3b761c85f3def4a8a69a34ee0a0b
Katie Spivey <katieaspivey@gmail.com>
2025-03-24 15:48:27 -0400

    slight error handling

--- program.py
+++ program.py
@@ -17,16 +17,17 @@ souped = BeautifulSoup(req.content, 'html.parser')
 #print(souped.prettify)
 
 # get needed type info based on html attributes
-type_info = souped.find("td", attrs={"width": "45px"})
-type_info_2 = type_info.find_next("td", attrs={"width": "45px"})
+if req:
+    type_info = souped.find("td", attrs={"width": "45px"})
+    type_info_2 = type_info.find_next("td", attrs={"width": "45px"})
 
-first_type = type_info.text.strip()
-if type_info_2.text.strip() != "Unknown" or None:
-    second_type = type_info_2.text.strip()
-else:
-    second_type = False
+    first_type = type_info.text.strip()
+    if type_info_2.text.strip() != "Unknown" or None:
+        second_type = type_info_2.text.strip()
+    else:
+        second_type = False
 
-if second_type:
-    print(f"{pokemon.capitalize()} is a {first_type} and {second_type} type Pokémon.")
-else:
-    print(f"{pokemon.capitalize()} is a {first_type} type Pokémon.")
+    if second_type:
+        print(f"{pokemon.capitalize()} is a {first_type} and {second_type} type Pokémon.")
+    else:
+        print(f"{pokemon.capitalize()} is a {first_type} type Pokémon.")
